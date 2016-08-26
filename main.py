c488cd0a83dcadf105949fd457aae5005ca2d445
Will <william.fanning1@gmail.com>
2016-08-26 16:15:12 -0500

    complete initial setup

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+
+class Post(db.Model):
+    title = db.StringProperty(required=True)
+    post = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+       self.write(self.render_str(template, **kw))
+
+
+class Main(Handler):
+
     def get(self):
-        self.response.write('Hello world!')
+        posts = db.GqlQuery('SELECT * FROM Post '
+                            'ORDER BY created DESC')
+        self.render('blog.html', posts=posts)
+
+    def post(self):
+        title = self.request.get('title')
+        post = self.request.get('post')
+
+        if title and post:
+            p = Post(title=title, post=post)
+            p.put()
+            self.redirect('/')
+        else:
+            error = 'Both title and post required'
+            self.render('newpost.html', title=title, post=post, error=error)
+
+
+class NewPost(Handler):
+    def get(self):
+        self.render('newpost.html')
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Main),
+    ('/newpost', NewPost)
 ], debug=True)
